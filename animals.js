1ec17dac12304dcbf0965ac6c8af6a55ec87d965
bonnichiwa <bonnie.pham@ryerson.ca>
2017-07-18 08:35:09 +0400

    now using inheritance

--- animals.js
+++ animals.js
@@ -1,13 +1,16 @@
 /**
  * Define the House object
- * @returns {Array} 
+ *
+ * no @returns
  */
 var House = function() {
   this.animals = [];
 }
 
+
 /**
  * When the door opens, all animals in the house will call for food
+ *
  * no @returns
  */
 House.prototype.openDoor = function() {
@@ -16,12 +19,15 @@ House.prototype.openDoor = function() {
   }
 }
 
+
 /**
  * Only animals can we adopted
+ *
+ * @param {Animal} animal
  * no @returns
  */
 House.prototype.adoptAnAnimal = function(animal) {
-  if(animal.isAnimal) {
+  if(animal instanceof Animal) {
     console.log("Welcome to the family, " + animal.name + " !");
     this.animals.push(animal);
   } else {
@@ -32,56 +38,46 @@ House.prototype.adoptAnAnimal = function(animal) {
 
 /**
  * Define the Animal Object
+ *
  * @params {String} name
- * @returns {Boolean} isAnimal
+ * no @returns
  */
-
 var Animal = function(name) {
   this.name = name;
-  this.isAnimal = true;
+  this.callForFood = function() {
+    console.log(this.name + " is calling for food");
+    console.log(this.greeting);
+  }
 }
 
 
 /**
  * Define the Cat Object
+ *
  * @params {String} name
- */
-
-var Cat = function(name) {
-  Animal.call(this, name);
-  this.name = name;
-  this.type = 'cat';
-}
-
-/**
- * What the cat says when it calls for food
  * no @returns
  */
-
-Cat.prototype.callForFood = function() {
-  console.log(this.name + " is calling for food");
-  console.log("Meow meow");
+var Cat = function(name) {
+    Animal.call(this, name);
+    this.name = name;
+    this.greeting = 'meow meow';
 }
+
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
 
 
 /**
  * Define the Dog Object
- * @params {string} name
+ *
+ * @params {String} name
  * no @returns
  */
-
 var Dog = function(name) {
   Animal.call(this, name);
   this.name = name;
-  this.type = 'dog';
+  this.greeting = 'ruff ruff';
 }
 
-/**
- * What the dog says when it calls for food
- * no @returns
- */
-
-Dog.prototype.callForFood = function() {
-  console.log(this.name + " is calling for food");
-  console.log("Ruff ruff");
-}
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
